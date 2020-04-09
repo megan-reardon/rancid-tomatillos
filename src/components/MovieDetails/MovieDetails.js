565781bca0b2cdc36c9d5c899ba2821cfc5bfde6
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-09 16:32:32 -0600

    Add styling position for movie details

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -11,13 +11,16 @@ class MovieDetails extends Component {
   render() {
     const { id, title, backdrop_path, poster_path, release_date, overview, average_rating } = this.props
     return (
-      <article className="movie-details-card">
-        <h1>{title}</h1>
-        <h3>Average rating: {average_rating}</h3>
-        <h3>Release date: {release_date}</h3>
-        <p>{overview}</p>
-        <img className="img-background" src={backdrop_path} alt={"image for " + title}/>
-        <img className="img-poster" src={poster_path} alt={"image for " + title}/>
+      <article className="movie-details-card" style={{ backgroundImage: `url(${backdrop_path})`}}>
+        <section className="img-poster">
+          <img src={poster_path} alt={"image for " + title}/>
+        </section>
+        <section className="movie-details">
+          <h1>{title}</h1>
+          <h3>Average rating: {average_rating}</h3>
+          <h3>Release date: {release_date}</h3>
+          <p>{overview}</p>
+        </section>
       </article>
     )
   }
