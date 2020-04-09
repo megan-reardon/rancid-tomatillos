a5daa6e410b5285f96a806b1a59220848403705d
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-09 14:32:36 -0600

    fixed hover styling

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 const MovieCard = ({ id, title, backdrop, averageRating }) => {
   return (
     <article className="movie-card">
-    <Link to={`/movies/${id}`}>
-      <img src={backdrop} alt={"image for " + title}/>
-    </Link>
-      <h1>{title}</h1>
-      <h2>Average Rating: {averageRating}</h2>
+      <Link to={`/movies/${id}`}>
+        <img src={backdrop} alt={"image for " + title}/>
+        <div className="movie-info-container">
+        <h1>{title}</h1>
+        <h2>Average Rating: {averageRating}</h2>
+      </div>
+      </Link>
     </article>
   )
 }
