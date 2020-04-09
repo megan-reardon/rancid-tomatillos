9a14ae1f51d648c0df8a491a8e23990a7d4a6d8f
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-09 14:26:05 -0600

    merging master

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -42,7 +42,6 @@ class App extends Component {
         path="/movies/:id/" exact
         render={({ match }) => {
           const selectedMovie = this.props.movies.find(movie => parseInt(match.params.id) === movie.id)
-          console.log(selectedMovie)
           return <MovieDetails
                   {...selectedMovie}
                  />
