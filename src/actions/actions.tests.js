9a7655b123d79f022347367432b140e2eac4ac37
Harry Borrelli <hborrelli@outlook.com>
2020-04-11 20:48:23 -0600

    GET_MOVIES test

---
+++ src/actions/actions.tests.js
@@ -0,0 +1,19 @@
+import * as actions from '.';
+
+describe('Actions Tests', () => {
+
+  it('should have a type of GET_MOVIES, and correct payload', () => {
+    const movie1 = {id: 1, title: 'First Movie'};
+    const movie2 = {id: 2, title: 'Second Movie'};
+
+    const expectedAction = {
+      type: 'GET_MOVIES',
+      movies: [movie1, movie2]
+    }
+
+    const result = actions.getMovies([movie1, movie2]);
+
+    expect(result).toEqual(expectedAction);
+  })
+
+});
