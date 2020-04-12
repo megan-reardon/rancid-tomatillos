7d64b81d13308971fc48355b27f46a323552213e
Harry Borrelli <hborrelli@outlook.com>
2020-04-12 15:32:44 -0600

    Refactor fetch user data and fetch movie ratings to api calls.

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,9 +1,30 @@
 const BASE_URL = 'https://rancid-tomatillos.herokuapp.com/api/v1';
 const MOVIES_ENDPOINT = '/movies/';
 const LOGIN_ENDPOINT = '/login/';
+const USERS = '/users/';
+const RATINGS_ENDPOINT = '/ratings/';
 
 export const apiFetchMovies = async () => {
   return await fetch(BASE_URL + MOVIES_ENDPOINT)
     .then(response => response.json())
       .catch(err => console.log(err.message))
+}
+
+export const apiFetchUserData = async (email, password) => {
+  return await fetch(BASE_URL + LOGIN_ENDPOINT,
+    {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(
+          {email: email, password: password}
+      ),
+    }
+  );
+}
+
+export const apiFetchRatings = (userId) => {
+  return fetch(BASE_URL + USERS + `${userId}` + RATINGS_ENDPOINT)
+    .then(response => response.json())
 }
