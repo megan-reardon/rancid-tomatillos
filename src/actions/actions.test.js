20c8c0076b7a0406b06118fbfa4bb314e22fb38d
Harry Borrelli <hborrelli@outlook.com>
2020-04-11 20:58:20 -0600

    save unstaged commits

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -10,10 +10,17 @@ describe('Actions Tests', () => {
       type: 'GET_MOVIES',
       movies: [movie1, movie2]
     }
-
     const result = actions.getMovies([movie1, movie2]);
 
     expect(result).toEqual(expectedAction);
+  });
+
+  it('should have type of LOGIN and correct payload', () => {
+    const userInfo = {
+      id: 1,
+      name: 'Lucy',
+      email: 'lucy@turing.io'
+    }
   })
 
 });
