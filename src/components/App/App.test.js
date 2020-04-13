40ea915d739eaa07c620d707c262538d28e9f576
Harry Borrelli <hborrelli@outlook.com>
2020-04-13 13:03:33 -0600

    Add first test to App.js

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from '../../reducers';
+
+describe('App Tests', () => {
+  it('Can render the app and display each component', async () => {
+    const store = createStore(rootReducer);
+
+    const { getByText, debug } = render(
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    )
+
+    expect(getByText('Login')).toBeInTheDocument();
+    await waitForElement(() => getByText('Bloodshot'))
+    expect(getByText('Bloodshot')).toBeInTheDocument();
+  });
+
 });
